2266f6b2ca7b06d32a13191bf9275d69fc658534
Hilbert Muniz de Andrade <hilbertmunizdeandrade@gmail.com>
2023-06-19 06:11:46 -0300

    script.js

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,3 +1,4 @@
+
 let saldo
 console.log(`Valor inicial do saldo: ${saldo}`)//placeholder 
 
@@ -7,7 +8,7 @@ saldo = saldo + 22.4
 console.log(`Valor do saldo após a primeira operação: ${saldo}`) //placeholder 
 
 // Operação numero 2
-saldo = saldo -12.34
+saldo = saldo - 12.34
 console.log(`Valor do saldo após a primeira operação: ${saldo}`) //placeholder 
 
 //Operação numero 3
@@ -17,7 +18,7 @@ console.log(`Saldo de cada irmão: ${saldo}`) //placeholder
 
 // Operação número 4
 
-saldo = saldo *3.42
+saldo = saldo * 3.42
 console.log(`Meu saldo após meu investimentoé: ${saldo}`) //placeholder
 
 // Operação número 5
@@ -27,3 +28,9 @@ console.log(`Agora eu tenho ${saldo} para comprar meus dadinhos`)
 
 console.log(2 % 5)
 console.log(2 / 5)
+
+let soma
+console.log(`a some total de um numero é: ${soma}`)
+
+soma = 15
+
